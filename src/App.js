18b738703e3f159e36958c0fadd162928e6c4076
Rich House <rich@richhouse.co.uk>
2021-02-27 08:40:22 +0000

    reverting previosu change

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Controls from "./components/Controls";
 import "./App.css";
 
 function App() {
-  console.log("update to v8");
+  console.log("update to v9");
   const videoRef = useRef(null);
   const searchParams = new URLSearchParams(window.location.search);
   const video_id = searchParams.get("video");
